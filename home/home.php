c946da9418971755942c43acf002d9960f9bba74
Sopr-Agil <34137553+Sopr-Agil@users.noreply.github.com>
2018-05-29 21:05:39 -0500

    imagenes portal

--- home/home.php
+++ home/home.php
@@ -1,19 +1,27 @@
 <?php              
+        require_once '../core/dao/ConnectionBD.php'; 
         require_once '../core/bo/Html.php';
         require '../constants/constants.php';
         require '../constants/constants_views.php';
-            
-        $uri = $_SERVER['REQUEST_URI'];
-        $html_file = str_replace(ROOT,'', $uri);      
-        $html_file = str_replace('/','', $html_file);
-        
-        handler($html_file);
-        
-        function handler($html_file) {            
-            $obj = new Html();
-            switch ($html_file) {
-                default: $obj->getHtml(VIEW_MAIN);
+    
+        $str_url = $_SERVER['REQUEST_URI'];        
+        $metod = str_replace(ROOT,'', $str_url);
+        $metod = str_replace(HOME,'', $metod);      
+        $metod = str_replace('/','', $metod);
+      
+        handler($metod);
+       
+        function handler($metod = '') {  
+               $obj = new Html();
+            switch ($metod) {
+                case VIEW_MONITOR:
+                    $obj->getHtml(VIEW_MONITOR);
+                    break;
+                default:
+                    $obj->getHtml(VIEW_MAIN);
             }//switch
+                        
             unset($obj);
-        }//handler
+        }
+
 ?>
